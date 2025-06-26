a08d1b66585852893530671006a6f159199d098c
suraj530 <kk3881648@gmail.com>
2025-06-26 16:56:20 +0530

    full message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,60 +1,61 @@
-import profile from "../assets/profile.png";
-import { Button } from "@mui/material";
+import { useState } from "react";
+import { messages } from "../assets/data";
 import DeleteIcon from "@mui/icons-material/Delete";
-export default function Message() {
-  return (
-    <div
-      style={{
-        width: "90%",
-        height: "205px",
-        border: "1px solid black",
-        marginTop: "20px",
-        marginLeft: "70px",
-      }}
-    >
-      <div>
-        <div
-          style={{ display: "flex", alignItems: "center", background: "pink" }}
-        >
-          <img src={profile} style={{ width: "40px", padding: "5px" }} />
-          <span style={{fontSize:'16px',fontWeight:530}}>suraj</span>
-          <div style={{ marginLeft: "1000px" }}>24-06-2025</div>
-          <div
-            style={{
-              marginLeft: "auto",
-              padding: "10px",
-              marginTop: "10px",
-              marginRight: "15px",
-            }}
-          >
-            12:00 PM
-          </div>
-        </div>
-      </div>
-      <div style={{ marginLeft: "35px", marginTop: "5px", fontSize:'16px',fontWeight:450 }}>Offer Latter</div>
+import { Button } from "@mui/material";
+import "./Message.css";
 
-    <div style={{display:'flex',alignItems:'center'}}>
-      <div
-        style={{
-          width: "87%",
-          background: "#81ecec",
-          height: "80px",
-          border: "1px solid black",
-          marginTop: "8px",
-          marginLeft: "35px",
-        }}
-      >
-        <div style={{ marginLeft: "10px", marginTop: "2px" }}>
-          Mr suraj you are select for Software Developement role in our company
-          dhunlay so you have to come tommorrow on time. Monday to friday is
-          working days in our company if you have any quiry contact us .{" "}
+export default function MessageList() {
+  const [openMessageId, setOpenMessageId] = useState(null);
+
+  return (
+    <div style={{ backgroundColor: "#000", minHeight: "100vh", padding: "20px" }}>
+      {messages.map((item) => (
+        <div key={item.id}>
+          <div className="message-wrapper">
+            <div className="message-header">
+              <img src={item.dp} alt="profile" />
+              <span className="message-username">{item.username}</span>
+              <div className="message-date-time">
+                <span>{item.date}</span>
+                <span>{item.time}</span>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  style={{
+                    borderRadius: "50%",
+                    minWidth: "35px",
+                    width: "35px",
+                    height: "35px",
+                    padding: 0,
+                    color: "white",
+                    borderColor: "white",
+                  }}
+                >
+                  <DeleteIcon fontSize="small" />
+                </Button>
+              </div>
+            </div>
+
+            <div className="message-title">{item.title}</div>
+
+            <div className="message-description">
+              <p style={{ marginBottom: "30px" }}>{item.description}</p>
+              <div
+                className="see-more-btn"
+                onClick={() =>
+                  setOpenMessageId(openMessageId === item.id ? null : item.id)
+                }
+              >
+                {openMessageId === item.id ? "see less" : "see more"}
+              </div>
+            </div>
+          </div>
+
+          {openMessageId === item.id && (
+            <div className="extra-message">{item.message}</div>
+          )}
         </div>
-      </div>
-      <Button variant="outlined" startIcon={<DeleteIcon />} style={{marginLeft:'10px',borderRadius:'5px'}}>
-        Delete
-      </Button>
-      </div>
-      
+      ))}
     </div>
   );
 }
